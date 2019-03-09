ed62daa48ebaec7fcc5c119afcc8009a0d6dfda1
Maskedheroe <296994179@qq.com>
2019-03-09 19:08:26 +0800

    pr

---
+++ app/src/main/java/com/example/hou/adapter/VoltAdapterDemo.kt
@@ -0,0 +1,41 @@
+package com.example.hou.adapter
+
+/**
+ * Target角色
+ * */
+interface FiveVolt {
+    public fun getVolt5(): Int
+}
+
+
+/**
+ *
+ * Adaptee角色 需要被转换的对象
+ *
+ * */
+
+public class Volt220 {
+
+    public fun getVolt220(): Int {
+        return 220
+    }
+
+}
+
+
+/**
+ * 对象适配器
+ * */
+public class VoltAdapter(adapter: Volt220) : FiveVolt {
+
+    val mVolt220: Volt220 = adapter
+
+
+    override fun getVolt5(): Int {
+        return 5
+    }
+
+    fun getVolt220(): Int =
+            mVolt220.getVolt220()
+
+}
